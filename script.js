5b3e2a46fbc2f5caa842ed83ce582da8ef3e4af7
sadia0004 <157726444+sadia0004@users.noreply.github.com>
2025-08-29 22:07:56 +0600

    add

--- script.js
+++ script.js
@@ -14,3 +14,35 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Copy Button Logic
+document.addEventListener('DOMContentLoaded', function () {
+    const copyButtons = document.querySelectorAll('.card button'); 
+    const copyCounter = document.querySelector('#copy-btn .counter'); 
+    let copyCount = parseInt(copyCounter.textContent); 
+
+    // Event listener for each copy button
+    copyButtons.forEach(button => {
+        if (button.querySelector('i') && button.querySelector('i').classList.contains('fa-copy')) { 
+            button.addEventListener('click', function () {
+                const card = this.closest('.card'); 
+                const serviceNumber = card.querySelector('.service-nmbr').textContent; 
+                const tempInput = document.createElement('input');
+                tempInput.value = serviceNumber;  
+                document.body.appendChild(tempInput);  
+                tempInput.select();
+                tempInput.setSelectionRange(0, 99999); 
+
+                document.execCommand('copy');
+
+           
+                document.body.removeChild(tempInput);
+
+                alert(`Emergency number saved: ${serviceNumber}`);
+
+                copyCount++;
+                copyCounter.textContent = copyCount; 
+            });
+        }
+    });
+});
+
